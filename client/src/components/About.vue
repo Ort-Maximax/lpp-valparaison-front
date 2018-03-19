4aa6da2a35a98f146a8e33c3902ef877303e95fd
Clément S <c.simonin@lyon.ort.asso.fr>
2018-03-19 21:04:36 +0100

    POC Okta auth

---
+++ client/src/components/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <h3>Even non-logged users can see this!</h3>
+  </div>
+</template>
---
+++ client/src/components/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Dashboard</h2>
+    <p>Oh shit you managed to log in, <b>{{name}}</b>!</p>
+  </div>
+</template>
+<script>
+  import auth from '../auth'
+  export default {
+    data () {
+      return {
+        name: auth.getName()
+      }
+    }
+  }
+</script>
