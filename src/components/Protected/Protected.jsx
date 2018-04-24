fcd2004c912ecc63ef203d43595c83be4202f685
Clément S <csimonin.dev@outlook.fr>
2018-04-24 14:27:25 +0200

    Ajout prototype Treeview

--- src/components/Protected/Protected.jsx
+++ src/components/Protected/Protected.jsx
@@ -1,3 +1,65 @@
-import React from 'react';
 
-export default () => <h3>Protected</h3>;
+import React from 'react';
+import interact from 'interactjs';
+import Grid from 'material-ui/Grid';
+import TreeExample from '../Tree/Tree';
+
+import './styles/Protected.css';
+
+function Protected() {
+  interact('.tree-resize')
+    .resizable({
+      // resize from all edges and corners
+      edges: {
+        right: true,
+      },
+
+      // keep the edges inside the parent
+      restrictEdges: {
+        outer: 'parent',
+        endOnly: true,
+      },
+
+      // minimum size
+      restrictSize: {
+        min: { width: 100, height: 50 },
+      },
+
+      inertia: true,
+    })
+    .on('resizemove', (event) => {
+      const target = event.target;
+      let x = (parseFloat(target.getAttribute('data-x')) || 0);
+      let y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+      // update the element's style
+      target.style.width = event.rect.width + 'px';
+      target.style.height = event.rect.height + 'px';
+
+      // translate when resizing from top or left edges
+      x += event.deltaRect.left;
+      y += event.deltaRect.top;
+
+      target.style.webkitTransform = target.style.transform =
+        'translate(' + x + 'px,' + y + 'px)';
+  });
+
+  return (
+    <Grid
+      style={{ margin: 0, width: '100%' }}
+      className="fullHeight"
+      container
+      spacing={16}
+      direction="row"
+    >
+      <div className="tree-resize">
+        <TreeExample />
+      </div>
+
+      <div className="content">
+        <h1> {'Ici l\'utilisateur peut acceder a ces fichiers'} </h1>
+      </div>
+    </Grid>
+  );
+}
+export default (Protected);
