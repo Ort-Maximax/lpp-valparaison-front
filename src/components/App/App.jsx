4b6aa9537cfcc6f19ccdf2238cfe34064f94ecfa
Clément S <csimonin.dev@outlook.fr>
2018-05-28 05:24:35 +0200

    fuck ma vie entiere

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,7 +19,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { musicPlaylist: [] };
-    this.apiUrl = 'https://still-reef-61170.herokuapp.com/';
+    this.apiUrl = 'https://still-reef-61170.herokuapp.com';
     this.onPlaylistChange = this.onPlaylistChange.bind(this);
   }
 
