910f8974d8c4fdd0cd8a2a6b1f4a1d6761b7bd14
Clément S <csimonin.dev@outlook.fr>
2018-06-11 00:09:19 +0200

    Url netlify

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,9 +19,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { musicPlaylist: [] };
-    // this.apiUrl = 'https://valparaiso-mockup.herokuapp.com'; // Pour netlify
+    this.apiUrl = 'https://valparaiso-mockup.herokuapp.com'; // Pour netlify
     // this.apiUrl = 'http://valparaiso.fr:3009'; // Pour dev
-    this.apiUrl = 'http://api.valparaiso.fr'; // Pour la prod
+    // this.apiUrl = 'http://api.valparaiso.fr'; // Pour la prod
     this.onPlaylistChange = this.onPlaylistChange.bind(this);
   }
 
