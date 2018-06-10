e512fb0dd799a88993cb8c37d81ad59e735a0295
Clément S <csimonin.dev@outlook.fr>
2018-06-10 15:49:23 +0200

    Home page revamp

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,29 +5,97 @@ import Grid from 'material-ui/Grid';
 import './styles/stars.css';
 import './styles/Home.css';
 
+import CrossPlatform from '../../img/components/CrossPlatform';
+import Cheap from '../../img/components/Coins';
+import Fast from '../../img/components/Rocket';
+
 const Home = () => (
   <section className="starsContainer">
     <div id="stars" />
     <div id="stars2" />
     <div id="stars3" />
-    <Paper className="mainPaper" elevation={4}>
-      <Grid container spacing={24}>
-        <Grid item xs={12}>
-          <h1>
-            {'Bienvenue à Valparaiso !'}
-          </h1>
-          <h2 id="hostname"> Hostname inconnu ...</h2>
-        </Grid>
+    <div className="mainPaper">
+      <Grid
+        container
+        direction="column"
+        justify="space-between"
+        alignItems="center"
+        style={{ width: '100%', height: '100%', margin: 0 }}
+      >
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <h1>
+              {'Bienvenue à Valparaiso !'}
+            </h1>
+            <h3> Tous vos médias, sur tous vos écrans</h3>
+            <h5 id="hostname"> Hostname inconnu ...</h5>
+          </Grid>
 
-        <Grid item xs={12}>
-          <h2>
-            {'C\'est plutot cool'}
-          </h2>
+          <Grid
+            className="cards"
+            item
+            container
+            xs={12}
+            direction="row"
+            alignItems="center"
+            justify="space-around"
+          >
+            <Paper className="paper">
+              <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+                style={{ width: '100%', margin: 0 }}
+              >
+                <CrossPlatform id="cross-platform" />
+                <h3> Crossplatform</h3>
+                <h5>Uploadez une fois, regardez partout !</h5>
+              </Grid>
+
+            </Paper>
+
+            <Paper className="paper">
+              <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+                style={{ width: '100%', margin: 0 }}
+              >
+                <Cheap id="cheap" />
+                <h3> Pas cher</h3>
+                <h5>Payez pour ce que vous utilisez, 5Go gratuit</h5>
+              </Grid>
+
+            </Paper>
+
+            <Paper className="paper">
+              <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+                style={{ width: '100%', margin: 0 }}
+              >
+                <Fast id="fast" />
+                <h3> Super rapide</h3>
+                <h5>Pas de limite, tout le monde a pleine vitesse !</h5>
+              </Grid>
+
+            </Paper>
+            <h1 style={{ marginTop: '10vh' }}> Découvrez nos offres et tarifs !</h1>
+
+          </Grid>
+          <div className="footer" >
+
+          Made with <span role="img" aria-label="heart">❤️</span> by Clément S.
+          </div>
         </Grid>
 
 
       </Grid>
-    </Paper>
+    </div>
   </section>
 );
 
