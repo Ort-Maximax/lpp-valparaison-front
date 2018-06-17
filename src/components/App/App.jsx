680b3f37ca9bb8ad40629fdad523bba8f98972f5
Clément S <csimonin.dev@outlook.fr>
2018-06-17 19:11:32 +0200

    build

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -21,8 +21,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { musicPlaylist: [], audioPlayer: false };
-    // this.apiUrl = 'https://valparaiso-mockup.herokuapp.com'; // Pour netlify
-    this.apiUrl = 'http://valparaiso.fr:3009'; // Pour dev
+    this.apiUrl = 'https://valparaiso-mockup.herokuapp.com'; // Pour netlify
+    // this.apiUrl = 'http://valparaiso.fr:3009'; // Pour dev
     // this.apiUrl = 'https://api.valparaiso.fr'; // Pour la prod
 
     this.onPlaylistChange = this.onPlaylistChange.bind(this);
